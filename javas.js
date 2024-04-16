c4616370f2ea6a4457d2770ba39ee590dd50ca46
Eduardo532 <112228065+Eduardo532@users.noreply.github.com>
2024-04-16 15:39:19 -0400

    avance

--- javas.js
+++ javas.js
@@ -1,11 +1,13 @@
 document.addEventListener('DOMContentLoaded', ()=>{
     const form = document.getElementById('form');
+    const conteiner = document.getElementById("conteiner")
+    const button = document.getElementById("button")
     form.addEventListener('submit', function (event){
         event.preventDefault();
         const nombre = form.elements['nombre'].value;
         const email = form.elements['email'].value;
         const telefono = form.elements['telefono'].value;
 
-        
+        const element = document.createElement("li");
     })
 })
